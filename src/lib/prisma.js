2cdc951596d6fe734eda42852ed98c999814ea40
Luca Gerlich <l.gerlich@fitseveneleven.de>
2026-01-27 01:54:40 +0100

    Upgrading to new tech

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,12 +1,17 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import pg from "pg";
+
+const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL });
+const adapter = new PrismaPg(pool);
 
 let prisma;
 
 if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
+  prisma = new PrismaClient({ adapter });
 } else {
   if (!global.prisma) {
-    global.prisma = new PrismaClient();
+    global.prisma = new PrismaClient({ adapter });
   }
   prisma = global.prisma;
 }
